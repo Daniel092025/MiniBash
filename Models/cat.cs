0d98bd8221e258572fd6b60308adba0be446f73a
Andreas092025 <andreas.092026@gmail.com>
2025-11-06 15:02:33 +0100

    Added cat and cp models

---
+++ Models/cat.cs
@@ -0,0 +1,33 @@
+using System;
+using System.IO;
+
+class Cat
+{
+    static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            // Leser direkte fra tastatur (stdin)
+            string input;
+            while ((input = Console.ReadLine()) != null)
+            {
+                Console.WriteLine(input);
+            }
+        }
+        else
+        {
+            foreach (var file in args)
+            {
+                if (File.Exists(file))
+                {
+                    string content = File.ReadAllText(file);
+                    Console.Write(content);
+                }
+                else
+                {
+                    Console.Error.WriteLine($"cat: {file}: No such file or directory");
+                }
+            }
+        }
+    }
+}
