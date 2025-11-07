427febe613cc42b35cb7066a54753a14671ca47a
brage092025 <brage092025@gmail.com>
2025-11-07 12:59:12 +0100

    updated ls.cs and added switch case in main to include ls

--- Models/ls.cs
+++ Models/ls.cs
@@ -1,63 +1,66 @@
-namespace MiniBash.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
 
-
-public class Ls
+namespace MiniBash.Models
 {
-    public static List()
+    public static class Ls
     {
-
-    }
-
-    static string FormatFileSizeHelper(double bytes)
-    {
-        const double KB = 1024;
-        const double MB = KB * 1024;
-        const double GB = MB * 1024;
-
-        return bytes switch
+        public static void List(string? path = null)
         {
-            >= GB => $"{bytes / GB:F2} GB",
-            >= MB => $"{bytes / MB:F2} MB",
-            >= KB => $"{bytes / KB:F2} KB",
-            _ => $"{bytes} Bytes"
-        };
+            // Default to users Documents folder if no path is provided
+            path ??= Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
 
+            Console.WriteLine($"Listing contents of: {path}");
+            Console.WriteLine();
 
+            if (!Directory.Exists(path))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Error: Directory '{path}' not found.");
+                Console.ResetColor();
+                return;
+            }
+
+            DirectoryInfo directory = new(path);
+
+            // List subdirectories
+            var folders = directory.GetDirectories();
+            foreach (var folder in folders)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkBlue;
+                Console.Write("/");
+                Console.ResetColor();
+                Console.WriteLine(folder.Name);
+            }
+
+            Console.WriteLine();
+
+            // List files
+            var files = directory.GetFiles();
+            foreach (var file in files)
+            {
+                string fileSize = FormatFileSizeHelper(file.Length);
+                Console.WriteLine($"File: {file.Name,-30}  {file.CreationTime:yyyy-MM-dd}  {file.Extension,-6}  {fileSize}");
+            }
+
+            Console.WriteLine();
+        }
+
+        // Helper method to make filesizes more readable. 
+        private static string FormatFileSizeHelper(double bytes)
+        {
+            const double KB = 1024;
+            const double MB = KB * 1024;
+            const double GB = MB * 1024;
+
+            return bytes switch
+            {
+                >= GB => $"{bytes / GB:F2} GB",
+                >= MB => $"{bytes / MB:F2} MB",
+                >= KB => $"{bytes / KB:F2} KB",
+                _ => $"{bytes} Bytes"
+            };
+        }
     }
-
 }
-
-
-/*
-
-
-
-
-string path = "C:/Users/Instruktor.P-02506/Documents/Kurs";
-Console.WriteLine($"GetFolderPath: {path}");
-DirectoryInfo ls = new(path);
-
-var folders = ls.GetDirectories();
-
-foreach (var folder in folders)
-{
-    Console.ForegroundColor = ConsoleColor.DarkBlue;
-    Console.Write("/");
-    Console.ResetColor();
-    Console.Write($"{folder.Name}\n");
-}
-
-
-var files = ls.GetFiles();
-
-foreach (var file in files)
-{
-    string fileSize = FormatFileSizeHelper(file.Length);
-    Console.WriteLine($"File: '{file.Name,-30}'   {file.CreationTime.Date:yyyy-MM-dd}  {file.Extension,-6}  {fileSize}");
-}
-
-   
-    
-}    
-
-*/
